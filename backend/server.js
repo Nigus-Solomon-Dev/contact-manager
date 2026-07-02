b2c8ae94679afa8d4df6181c85f82c9c0be5ae0b
NS <Nigus.Solomon.Dev@outlook.com>
2026-07-02 22:59:11 +0300

    Initialize backend with Express, MongoDB, and dotenv

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express=require('express');
+const mongoose=require('mongoose');
+const cors=require('cors');
+const authRouter=require('./routes/authRoutes')
+
+
+const app=express();
+
+//allow front end to connect
+app.use(cors());
+app.use(express.json())
+app.use('/api/auth',authRouter)
+//connecting with mongoDB
+mongoose.connect(process.env.MONGODB_URI).
+then(()=>console.log('mongoDB is connected'))
+.catch((err)=>console.log('mongoDB is not connected',err)
+);
+app.get('/api/test',(req,res)=>{
+  res.send('backend is working');
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+  console.log(`server is running ${PORT}`);
+  
+})
